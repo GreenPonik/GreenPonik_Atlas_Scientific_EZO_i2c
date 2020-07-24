decebf7acbe49538790196242c406da66dc9867b
MkLHX <mickael.lehoux@gmail.com>
2020-07-24 18:27:26 +0200

    fix example

--- example/read_ec_with_compensed_temp.py
+++ example/read_ec_with_compensed_temp.py
@@ -1,6 +1,6 @@
 from GreenPonik_Atlas_Scientific_i2c import AtlasI2c
 import commons_i2c
-from GreenPonik_thermistor_10k import GreenPonik_thermistor_10k
+from GreenPonik_thermistor_10k import read_temp
 
 if __name__ == "__main__":
     print("get device infos")
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     print(commons_i2c.get_device_info(ec_i2c))
     print("get current temperature compensated")
     print(commons_i2c.get_temperature(ec_i2c))
-    t = GreenPonik_thermistor_10k.read_temp()
+    t = read_temp()
     print(commons_i2c.set_temperature(ec_i2c, t))
     ec = commons_i2c.get_read(ec_i2c)
     print("current ec is %.2f" % ec)
