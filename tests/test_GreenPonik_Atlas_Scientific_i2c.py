315fe874ab9c4152ec6a416e73748f038b6339a0
MkLHX <mickael.lehoux@gmail.com>
2020-07-29 18:16:09 +0200

    change called modules

--- tests/test_GreenPonik_Atlas_Scientific_i2c.py
+++ tests/test_GreenPonik_Atlas_Scientific_i2c.py
@@ -1,6 +1,6 @@
 import unittest
-from GreenPonik_Atlas_Scientific_i2c import AtlasI2c
-from GreenPonik_Atlas_Scientific_i2c import commons_i2c
+from GreenPonik_Atlas_Scientific_i2c.GreenPonik_Atlas_Scientific_i2c import AtlasI2c, CommonsI2c
+
 
 # TODO implement mock to emulate I2C bus
 
@@ -10,13 +10,13 @@ class TestGreenPonik_altals_Scientifics_I2C(unittest.TestCase):
     def test_get_device_info(self):
         device = AtlasI2c(
             address=AtlasI2c.AS_SENSORS_ADDS_TXT_TO_DECIMAL['EC'], moduletype="EC", name="EC")
-        infos = commons_i2c.get_device_info(device)
+        infos = CommonsI2c.get_device_info(device)
         self.assertEqual(infos, "bob")
 
     def test_get_read(self):
         device = AtlasI2c(
             address=AtlasI2c.AS_SENSORS_ADDS_TXT_TO_DECIMAL['EC'], moduletype="EC", name="EC")
-        value = commons_i2c.get_read(device)
+        value = CommonsI2c.get_read(device)
         self.assertEqual(value, "tutu")
 
 
