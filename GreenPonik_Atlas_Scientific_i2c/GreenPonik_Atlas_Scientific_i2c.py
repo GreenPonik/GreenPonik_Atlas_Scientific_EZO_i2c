f8423994c69b9767f83da7f7be640e66bcc2317e
Mickael <mickael.lehoux@greenponik.com>
2020-09-08 17:00:07 +0200

    adapt with smbus library

--- GreenPonik_Atlas_Scientific_i2c/GreenPonik_Atlas_Scientific_i2c.py
+++ GreenPonik_Atlas_Scientific_i2c/GreenPonik_Atlas_Scientific_i2c.py
@@ -20,6 +20,263 @@ import fcntl
 import time
 import copy
 
+import adafruit_bus_device.i2c_device as i2c_device
+from adafruit_extended_bus import ExtendedI2C as I2C
+
+
+# class AtlasI2c:
+#     """@brief
+#     Array value of each EZO sensors decimal addresses
+#     """
+#     AS_SENSORS_ADDS_DECIMAL = {
+#         97,
+#         98,
+#         99,
+#         100,
+#         102,
+#         103,
+#     }
+#     """@brief
+#     Array key=>value for each sensors name i2c decimal addresses
+#     """
+#     AS_SENSORS_ADDS_TXT_TO_DECIMAL = {
+#         'DO': 97,
+#         'ORP': 98,
+#         'PH': 99,
+#         'EC': 100,
+#         'RTD': 102,
+#         'PMP': 103,
+#     }
+#     """@brief
+#     Array key=>value for each sensors name i2c hexa addresses
+#     """
+#     AS_SENSORS_ADDS_TXT_TO_HEXA = {
+#         'DO': 0x61,
+#         'ORP': 0x62,
+#         'PH': 0x63,
+#         'EC': 0x64,
+#         'RTD': 0x66,
+#         'PMP': 0x67,
+#     }
+#     """@brief
+#     Array key=>value for each i2c hexa to decimal addresses
+#     """
+#     AS_SENSORS_ADDS_HEXA_TO_DECIMAL = {
+#         0x61: 97,
+#         0x62: 98,
+#         0x63: 99,
+#         0x64: 100,
+#         0x66: 102,
+#         0x67: 103,
+#     }
+#     """@brief
+#     Array key=>value for each sensors restart code
+#     """
+#     AS_RESTART_CODES = {
+#         'P': 'powered off',
+#         'S': 'software reset',
+#         'B': 'brown out',
+#         'W': 'watchdog',
+#         'U': 'unknown',
+#     }
+#     """@brief
+#     Array key=>value for each response code
+#     """
+#     AS_RESPONSE_CODE_TXT = {
+#         1: 'successful request',
+#         2: 'syntax error',
+#         254: 'still processing, not ready',
+#         255: 'no data to send',
+#     }
+
+#     # the timeout needed to query readings and calibrations
+#     LONG_TIMEOUT = 1.5
+#     # timeout for regular commands
+#     SHORT_TIMEOUT = .3
+#     # the default bus for I2C on the newer Raspberry Pis,
+#     # certain older boards use bus 0
+#     DEFAULT_BUS = 1
+#     # the default address for the sensor
+#     DEFAULT_ADDR = 98
+#     LONG_TIMEOUT_COMMANDS = ("R", "CAL")
+#     SLEEP_COMMANDS = ("SLEEP", )
+
+#     def __init__(self, address=None, moduletype="", name="", bus=None):
+#         """
+#         open two file streams, one for reading and one for writing
+#         the specific I2C channel is selected with bus
+#         it is usually 1, except for older revisions where its 0
+#         wb and rb indicate binary read and write
+#         """
+#         self._address = address or self.DEFAULT_ADDR
+#         self.bus = bus or self.DEFAULT_BUS
+#         self._long_timeout = self.LONG_TIMEOUT
+#         self._short_timeout = self.SHORT_TIMEOUT
+#         self.file_read = io.open(file="/dev/i2c-{}".format(self.bus),
+#                                  mode="rb",
+#                                  buffering=0)
+#         self.file_write = io.open(file="/dev/i2c-{}".format(self.bus),
+#                                   mode="wb",
+#                                   buffering=0)
+#         self.set_i2c_address(self._address)
+#         self._name = name
+#         self._module = moduletype
+
+#     @property
+#     def long_timeout(self):
+#         return self._long_timeout
+
+#     @property
+#     def short_timeout(self):
+#         return self._short_timeout
+
+#     @property
+#     def name(self):
+#         return self._name
+
+#     @property
+#     def address(self):
+#         return self._address
+
+#     @property
+#     def moduletype(self):
+#         return self._module
+
+#     def set_i2c_address(self, addr):
+#         """
+#         @brief set the I2C communications to the slave specified by the address
+#         the commands for I2C dev using the ioctl functions are specified in
+#         the i2c-dev.h file from i2c-tools
+#         """
+#         I2C_SLAVE = 0x703
+#         fcntl.ioctl(self.file_read, I2C_SLAVE, addr)
+#         fcntl.ioctl(self.file_write, I2C_SLAVE, addr)
+#         self._address = addr
+
+#     def write(self, cmd):
+#         """
+#         @brief appends the null character and sends the string over I2C
+#         """
+#         cmd += "\00"
+#         self.file_write.write(cmd.encode('latin-1'))
+
+#     def handle_raspi_glitch(self, response):
+#         """
+#         @brief Change MSB to 0 for all received characters except the first
+#         and get a list of characters
+#         NOTE: having to change the MSB to 0 is a glitch in the raspberry pi,
+#         and you shouldn't have to do this!
+#         """
+#         if self.app_using_python_two():
+#             return list(map(lambda x: chr(ord(x) & ~0x80), list(response)))
+#         else:
+#             return list(map(lambda x: chr(x & ~0x80), list(response)))
+
+#     def app_using_python_two(self):
+#         return sys.version_info[0] < 3
+
+#     def get_response(self, raw_data):
+#         if self.app_using_python_two():
+#             response = [i for i in raw_data if i != '\x00']
+#         else:
+#             response = raw_data
+
+#         return response
+
+#     def response_valid(self, response):
+#         valid = True
+#         error_code = None
+#         if(len(response) > 0):
+
+#             if self.app_using_python_two():
+#                 error_code = str(ord(response[0]))
+#             else:
+#                 error_code = str(response[0])
+
+#             if error_code != '1':  # 1:
+#                 valid = False
+
+#         return valid, error_code
+
+#     def get_device_info(self):
+#         """
+#         @brief get the device info
+#         @return string moduletype address name
+#         """
+#         if(self._name == ""):
+#             return self._module + " " + str(self.address)
+#         else:
+#             return self._module + " " + str(self.address) + " " + self._name
+
+#     def read(self, num_of_bytes=31):
+#         """
+#         @brief reads a specified number of bytes from I2C,
+#         then parses and displays the result
+#         """
+#         raw_data = self.file_read.read(num_of_bytes)
+#         response = self.get_response(raw_data=raw_data)
+#         # print(response)
+#         is_valid, error_code = self.response_valid(response=response)
+
+#         if is_valid:
+#             char_list = self.handle_raspi_glitch(response[1:])
+#             # result = "Success " + self.get_device_info()
+#             # + ": "
+#             # + str(''.join(char_list))
+#             # result = "Success: " +  str(''.join(char_list))
+#             result = "Success %s: %s" % (
+#                 self.get_device_info(),
+#                 str(''.join(char_list))
+#             )
+#         else:
+#             result = "Error " + self.get_device_info() + ": " + error_code
+
+#         return result
+
+#     def get_command_timeout(self, command):
+#         timeout = None
+#         if command.upper().startswith(self.LONG_TIMEOUT_COMMANDS):
+#             timeout = self._long_timeout
+#         elif not command.upper().startswith(self.SLEEP_COMMANDS):
+#             timeout = self.short_timeout
+
+#         return timeout
+
+#     def query(self, command):
+#         """
+#         @brief write a command to the board, wait the correct timeout,
+#         and read the response
+#         """
+#         self.write(command)
+#         current_timeout = self.get_command_timeout(command=command)
+#         if not current_timeout:
+#             return "sleep mode"
+#         else:
+#             time.sleep(current_timeout)
+#             return self.read()
+
+#     def close(self):
+#         self.file_read.close()
+#         self.file_write.close()
+
+#     def list_i2c_devices(self):
+#     """
+#     @brief save the current address so we can restore it after
+#     """
+#     prev_addr = copy.deepcopy(self._address)
+#     i2c_devices = []
+#     for i in range(0, 128):
+#         try:
+#             self.set_i2c_address(i)
+#             self.read(1)
+#             i2c_devices.append(i)
+#         except IOError:
+#             pass
+#     # restore the address we were using
+#     self.set_i2c_address(prev_addr)
+
+#     return i2c_devices
+
 
 class AtlasI2c:
     """@brief
@@ -86,149 +343,42 @@ class AtlasI2c:
         255: 'no data to send',
     }
 
-    # the timeout needed to query readings and calibrations
-    LONG_TIMEOUT = 1.5
-    # timeout for regular commands
-    SHORT_TIMEOUT = .3
     # the default bus for I2C on the newer Raspberry Pis,
     # certain older boards use bus 0
     DEFAULT_BUS = 1
     # the default address for the sensor
-    DEFAULT_ADDRESS = 98
+    DEFAULT_ADDR = 98
+
+    # the timeout needed to query readings and calibrations
+    LONG_TIMEOUT = 1.5
+    # timeout for regular commands
+    SHORT_TIMEOUT = .3
+
     LONG_TIMEOUT_COMMANDS = ("R", "CAL")
     SLEEP_COMMANDS = ("SLEEP", )
 
-    def __init__(self, address=None, moduletype="", name="", bus=None):
+    def __init__(self, addr=DEFAULT_ADDR, bus=DEFAULT_BUS, moduletype="", name=""):
         """
         open two file streams, one for reading and one for writing
         the specific I2C channel is selected with bus
         it is usually 1, except for older revisions where its 0
         wb and rb indicate binary read and write
         """
-        self._address = address or self.DEFAULT_ADDRESS
-        self.bus = bus or self.DEFAULT_BUS
+        self._address = addr
+        self.bus = bus
         self._long_timeout = self.LONG_TIMEOUT
         self._short_timeout = self.SHORT_TIMEOUT
-        self.file_read = io.open(file="/dev/i2c-{}".format(self.bus),
-                                 mode="rb",
-                                 buffering=0)
-        self.file_write = io.open(file="/dev/i2c-{}".format(self.bus),
-                                  mode="wb",
-                                  buffering=0)
-        self.set_i2c_address(self._address)
+        # self.file_read = io.open(file="/dev/i2c-{}".format(self.bus),
+        #                          mode="rb",
+        #                          buffering=0)
+        # self.file_write = io.open(file="/dev/i2c-{}".format(self.bus),
+        #                           mode="wb",
+        #                           buffering=0)
+        self.i2c_device = i2c_device.I2CDevice(self.bus, self._address)
         self._name = name
         self._module = moduletype
-
-    @property
-    def long_timeout(self):
-        return self._long_timeout
-
-    @property
-    def short_timeout(self):
-        return self._short_timeout
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def address(self):
-        return self._address
-
-    @property
-    def moduletype(self):
-        return self._module
-
-    def set_i2c_address(self, addr):
-        """
-        @brief set the I2C communications to the slave specified by the address
-        the commands for I2C dev using the ioctl functions are specified in
-        the i2c-dev.h file from i2c-tools
-        """
-        I2C_SLAVE = 0x703
-        fcntl.ioctl(self.file_read, I2C_SLAVE, addr)
-        fcntl.ioctl(self.file_write, I2C_SLAVE, addr)
-        self._address = addr
-
-    def write(self, cmd):
-        """
-        @brief appends the null character and sends the string over I2C
-        """
-        cmd += "\00"
-        self.file_write.write(cmd.encode('latin-1'))
-
-    def handle_raspi_glitch(self, response):
-        """
-        @brief Change MSB to 0 for all received characters except the first
-        and get a list of characters
-        NOTE: having to change the MSB to 0 is a glitch in the raspberry pi,
-        and you shouldn't have to do this!
-        """
-        if self.app_using_python_two():
-            return list(map(lambda x: chr(ord(x) & ~0x80), list(response)))
-        else:
-            return list(map(lambda x: chr(x & ~0x80), list(response)))
-
-    def app_using_python_two(self):
-        return sys.version_info[0] < 3
-
-    def get_response(self, raw_data):
-        if self.app_using_python_two():
-            response = [i for i in raw_data if i != '\x00']
-        else:
-            response = raw_data
-
-        return response
-
-    def response_valid(self, response):
-        valid = True
-        error_code = None
-        if(len(response) > 0):
-
-            if self.app_using_python_two():
-                error_code = str(ord(response[0]))
-            else:
-                error_code = str(response[0])
-
-            if error_code != '1':  # 1:
-                valid = False
-
-        return valid, error_code
-
-    def get_device_info(self):
-        """
-        @brief get the device info
-        @return string moduletype address name
-        """
-        if(self._name == ""):
-            return self._module + " " + str(self.address)
-        else:
-            return self._module + " " + str(self.address) + " " + self._name
-
-    def read(self, num_of_bytes=31):
-        """
-        @brief reads a specified number of bytes from I2C,
-        then parses and displays the result
-        """
-        raw_data = self.file_read.read(num_of_bytes)
-        response = self.get_response(raw_data=raw_data)
-        # print(response)
-        is_valid, error_code = self.response_valid(response=response)
-
-        if is_valid:
-            char_list = self.handle_raspi_glitch(response[1:])
-            # result = "Success " + self.get_device_info()
-            # + ": "
-            # + str(''.join(char_list))
-            # result = "Success: " +  str(''.join(char_list))
-            result = "Success %s: %s" % (
-                self.get_device_info(),
-                str(''.join(char_list))
-            )
-        else:
-            result = "Error " + self.get_device_info() + ": " + error_code
-
-        return result
+        self._buffer = bytearray(2)
+        self._settings_byte = 0
 
     def get_command_timeout(self, command):
         timeout = None
@@ -244,35 +394,29 @@ class AtlasI2c:
         @brief write a command to the board, wait the correct timeout,
         and read the response
         """
-        self.write(command)
+        self._write(command)
         current_timeout = self.get_command_timeout(command=command)
         if not current_timeout:
             return "sleep mode"
         else:
             time.sleep(current_timeout)
-            return self.read()
+            return self._read()
 
-    def close(self):
-        self.file_read.close()
-        self.file_write.close()
-
-    def list_i2c_devices(self):
+    def _read(self):
         """
-        @brief save the current address so we can restore it after
+        @brief reads a specified number of bytes from I2C,
+        then parses and displays the result
         """
-        prev_addr = copy.deepcopy(self._address)
-        i2c_devices = []
-        for i in range(0, 128):
-            try:
-                self.set_i2c_address(i)
-                self.read(1)
-                i2c_devices.append(i)
-            except IOError:
-                pass
-        # restore the address we were using
-        self.set_i2c_address(prev_addr)
+        self._buffer[0] = 0
+        self._buffer[1] = 0
+        with self.i2c_device as i2c:
+            i2c.readinto(self._buffer,)
+        return self._buffer
 
-        return i2c_devices
+    def _write(self, cmd_byte):
+        self._buffer[0] = cmd_byte
+        with self.i2c_device as i2c:
+            i2c.write(self._buffer, end=1)
 
 
 class CommonsI2c:
