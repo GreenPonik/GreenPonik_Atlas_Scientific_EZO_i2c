036e5584cc6f12884846feb31a6e57e5dd38d0d0
Mickael <mickael.lehoux@greenponik.com>
2020-09-09 19:32:12 +0200

    fix

--- GreenPonik_Atlas_Scientific_i2c/GreenPonik_Atlas_Scientific_i2c.py
+++ GreenPonik_Atlas_Scientific_i2c/GreenPonik_Atlas_Scientific_i2c.py
@@ -29,6 +29,7 @@ class AtlasI2c:
     """@brief
     Array key=>value for each EZO sensors i2c hexa addresses
     """
+
     ADDR_EZO_HEXA = {
         0x61,  # DO
         0x62,  # ORP
@@ -39,27 +40,27 @@ class AtlasI2c:
     Array value of each EZO sensors i2c decimal addresses
     """
     ADDR_EZO_DECIMAL = {
-        97,   # DO
-        98,   # ORP
-        99,   # PH
+        97,  # DO
+        98,  # ORP
+        99,  # PH
         100,  # EC
     }
     """@brief
     Array key=>value for each EZO sensors name i2c hexa addresses
     """
     ADDR_EZO_TXT_TO_HEXA = {
-        'DO': 0x61,
-        'ORP': 0x62,
-        'PH': 0x63,
-        'EC': 0x64,
+        "DO": 0x61,
+        "ORP": 0x62,
+        "PH": 0x63,
+        "EC": 0x64,
     }
     """@brief
     Array key=>value for each EZO sensors i2c hexa to decimal addresses
     """
     ADDR_EZO_HEXA_TO_DECIMAL = {
-        0x61: 97,   # DO
-        0x62: 98,   # ORP
-        0x63: 99,   # PH
+        0x61: 97,  # DO
+        0x62: 98,  # ORP
+        0x63: 99,  # PH
         0x64: 100,  # EC
     }
     """@brief
@@ -75,27 +76,27 @@ class AtlasI2c:
     Array value of each OEM sensors decimal addresses
     """
     ADDR_OEM_DECIMAL = {
-        100,   # EC
-        101,   # PH
-        102,   # ORP
-        103,   # DO
+        100,  # EC
+        101,  # PH
+        102,  # ORP
+        103,  # DO
     }
     """@brief
     Array key=>value for each OEM sensors name i2c hexa addresses
     """
     ADDR_OEM_TXT_TO_HEXA = {
-        'EC': 0x64,
-        'PH': 0x65,
-        'ORP': 0x66,
-        'DO': 0x67,
+        "EC": 0x64,
+        "PH": 0x65,
+        "ORP": 0x66,
+        "DO": 0x67,
     }
     """@brief
     Array key=>value for each OEM sensors i2c hexa to decimal addresses
     """
     ADDR_OEM_HEXA_TO_DECIMAL = {
-        0x64: 100,   # DO
-        0x65: 101,   # ORP
-        0x66: 102,   # PH
+        0x64: 100,  # DO
+        0x65: 101,  # ORP
+        0x66: 102,  # PH
         0x67: 103,  # EC
     }
 
@@ -105,71 +106,71 @@ class AtlasI2c:
     FOUR_BYTE_READ = 0x04
 
     OEM_EC_REGISTERS = {
-        'device_type': 0x00,
-        'device_firmware': 0x01,
-        'device_addr_lock': 0x02,
-        'device_addr': 0x03,
-        'device_intr': 0x04,
-        'device_led': 0x05,
-        'device_sleep': 0x06,
-        'device_new_reading': 0x07,
-        'device_probe_type_msb': 0x08,  # 0x08 - 0x09 2 registers
-        'device_probe_type_lsb': 0x09,  # 0x08 - 0x09 2 registers
-        'device_calibration_value_msb': 0x0A,  # 0x0A - 0x0D 4 registers
-        'device_calibration_value_high': 0x0B,  # 0x0A - 0x0D 4 registers
-        'device_calibration_value_low': 0x0C,  # 0x0A - 0x0D 4 registers
-        'device_calibration_value_lsb': 0x0D,  # 0x0A - 0x0D 4 registers
-        'device_calibration_request': 0x0E,
-        'device_calibration_confirm': 0x0F,
-        'device_temperature_comp_msb': 0x10,   # 0x10 - 0x13 4 registers
-        'device_temperature_comp_high': 0x11,   # 0x10 - 0x13 4 registers
-        'device_temperature_comp_low': 0x12,   # 0x10 - 0x13 4 registers
-        'device_temperature_comp_lsb': 0x13,   # 0x10 - 0x13 4 registers
-        'device_temperature_confirm_msb': 0x14,   # 0x14 - 0x17 4 registers
-        'device_temperature_confirm_high': 0x15,   # 0x14 - 0x17 4 registers
-        'device_temperature_confirm_low': 0x16,   # 0x14 - 0x17 4 registers
-        'device_temperature_confirm_lsb': 0x17,   # 0x14 - 0x17 4 registers
-        'device_ec_msb': 0x18,   # 0x18 - 0x1B 4 registers
-        'device_ec_high': 0x19,   # 0x18 - 0x1B 4 registers
-        'device_ec_low': 0x20,   # 0x18 - 0x1B 4 registers
-        'device_ec_lsb': 0x21,   # 0x18 - 0x1B 4 registers
-        'device_tds_msb': 0x1C,   # 0x1C - 0x1F 3 registers
-        'device_tds_high': 0x1D,   # 0x1C - 0x1F 3 registers
-        'device_tds_low': 0x1E,   # 0x1C - 0x1F 3 registers
-        'device_tds_lsb': 0x1F,   # 0x1C - 0x1F 3 registers
-        'device_salinity_msb': 0x20,   # 0x20 - 0x23 4 registers
-        'device_salinity_high': 0x21,   # 0x20 - 0x23 4 registers
-        'device_salinity_low': 0x22,   # 0x20 - 0x23 4 registers
-        'device_salinity_lsb': 0x23,   # 0x20 - 0x23 4 registers
+        "device_type": 0x00,
+        "device_firmware": 0x01,
+        "device_addr_lock": 0x02,
+        "device_addr": 0x03,
+        "device_intr": 0x04,
+        "device_led": 0x05,
+        "device_sleep": 0x06,
+        "device_new_reading": 0x07,
+        "device_probe_type_msb": 0x08,  # 0x08 - 0x09 2 registers
+        "device_probe_type_lsb": 0x09,  # 0x08 - 0x09 2 registers
+        "device_calibration_value_msb": 0x0A,  # 0x0A - 0x0D 4 registers
+        "device_calibration_value_high": 0x0B,  # 0x0A - 0x0D 4 registers
+        "device_calibration_value_low": 0x0C,  # 0x0A - 0x0D 4 registers
+        "device_calibration_value_lsb": 0x0D,  # 0x0A - 0x0D 4 registers
+        "device_calibration_request": 0x0E,
+        "device_calibration_confirm": 0x0F,
+        "device_temperature_comp_msb": 0x10,  # 0x10 - 0x13 4 registers
+        "device_temperature_comp_high": 0x11,  # 0x10 - 0x13 4 registers
+        "device_temperature_comp_low": 0x12,  # 0x10 - 0x13 4 registers
+        "device_temperature_comp_lsb": 0x13,  # 0x10 - 0x13 4 registers
+        "device_temperature_confirm_msb": 0x14,  # 0x14 - 0x17 4 registers
+        "device_temperature_confirm_high": 0x15,  # 0x14 - 0x17 4 registers
+        "device_temperature_confirm_low": 0x16,  # 0x14 - 0x17 4 registers
+        "device_temperature_confirm_lsb": 0x17,  # 0x14 - 0x17 4 registers
+        "device_ec_msb": 0x18,  # 0x18 - 0x1B 4 registers
+        "device_ec_high": 0x19,  # 0x18 - 0x1B 4 registers
+        "device_ec_low": 0x20,  # 0x18 - 0x1B 4 registers
+        "device_ec_lsb": 0x21,  # 0x18 - 0x1B 4 registers
+        "device_tds_msb": 0x1C,  # 0x1C - 0x1F 3 registers
+        "device_tds_high": 0x1D,  # 0x1C - 0x1F 3 registers
+        "device_tds_low": 0x1E,  # 0x1C - 0x1F 3 registers
+        "device_tds_lsb": 0x1F,  # 0x1C - 0x1F 3 registers
+        "device_salinity_msb": 0x20,  # 0x20 - 0x23 4 registers
+        "device_salinity_high": 0x21,  # 0x20 - 0x23 4 registers
+        "device_salinity_low": 0x22,  # 0x20 - 0x23 4 registers
+        "device_salinity_lsb": 0x23,  # 0x20 - 0x23 4 registers
     }
 
     OEM_PH_REGISTERS = {
-        'device_type': 0x00,
-        'device_firmware': 0x01,
-        'device_addr_lock': 0x02,
-        'device_addr': 0x03,
-        'device_intr': 0x04,
-        'device_led': 0x05,
-        'device_sleep': 0x06,
-        'device_new_reading': 0x07,
-        'device_calibration_msb': 0x08,  # 0x08 - 0x0B 4 registers
-        'device_calibration_high': 0x09,  # 0x08 - 0x0B 4 registers
-        'device_calibration_low': 0x0A,  # 0x08 - 0x0B 4 registers
-        'device_calibration_lsb': 0x0B,  # 0x08 - 0x0B 4 registers
-        'device_calibration_request': 0x0C,
-        'device_calibration_confirm': 0x0D,
-        'device_temperature_comp_msb': 0x0E,   # 0x0E - 0x11 4 registers
-        'device_temperature_comp_high': 0x0F,   # 0x0E - 0x11 4 registers
-        'device_temperature_comp_low': 0x10,   # 0x0E - 0x11 4 registers
-        'device_temperature_comp_lsb': 0x11,   # 0x0E - 0x11 4 registers
-        'device_temperature_confirm_msb': 0x12,   # 0x12 - 0x15 4 registers
-        'device_temperature_confirm_high': 0x13,   # 0x12 - 0x15 4 registers
-        'device_temperature_confirm_low': 0x14,   # 0x12 - 0x15 4 registers
-        'device_temperature_confirm_lsb': 0x15,   # 0x12 - 0x15 4 registers
-        'device_ph_read_msb': 0x16,   # 0x16 - 0x19 4 registers
-        'device_ph_read_high': 0x16,   # 0x16 - 0x19 4 registers
-        'device_ph_read_low': 0x16,   # 0x16 - 0x19 4 registers
-        'device_ph_read_lsb': 0x16,   # 0x16 - 0x19 4 registers
+        "device_type": 0x00,
+        "device_firmware": 0x01,
+        "device_addr_lock": 0x02,
+        "device_addr": 0x03,
+        "device_intr": 0x04,
+        "device_led": 0x05,
+        "device_sleep": 0x06,
+        "device_new_reading": 0x07,
+        "device_calibration_msb": 0x08,  # 0x08 - 0x0B 4 registers
+        "device_calibration_high": 0x09,  # 0x08 - 0x0B 4 registers
+        "device_calibration_low": 0x0A,  # 0x08 - 0x0B 4 registers
+        "device_calibration_lsb": 0x0B,  # 0x08 - 0x0B 4 registers
+        "device_calibration_request": 0x0C,
+        "device_calibration_confirm": 0x0D,
+        "device_temperature_comp_msb": 0x0E,  # 0x0E - 0x11 4 registers
+        "device_temperature_comp_high": 0x0F,  # 0x0E - 0x11 4 registers
+        "device_temperature_comp_low": 0x10,  # 0x0E - 0x11 4 registers
+        "device_temperature_comp_lsb": 0x11,  # 0x0E - 0x11 4 registers
+        "device_temperature_confirm_msb": 0x12,  # 0x12 - 0x15 4 registers
+        "device_temperature_confirm_high": 0x13,  # 0x12 - 0x15 4 registers
+        "device_temperature_confirm_low": 0x14,  # 0x12 - 0x15 4 registers
+        "device_temperature_confirm_lsb": 0x15,  # 0x12 - 0x15 4 registers
+        "device_ph_read_msb": 0x16,  # 0x16 - 0x19 4 registers
+        "device_ph_read_high": 0x16,  # 0x16 - 0x19 4 registers
+        "device_ph_read_low": 0x16,  # 0x16 - 0x19 4 registers
+        "device_ph_read_lsb": 0x16,  # 0x16 - 0x19 4 registers
     }
 
     # TODO don't give a default address and provide an error when nothing is provided
@@ -182,10 +183,10 @@ class AtlasI2c:
     # the timeout needed to query readings and calibrations
     LONG_TIMEOUT = 1.5
     # timeout for regular commands
-    SHORT_TIMEOUT = .3
+    SHORT_TIMEOUT = 0.3
 
     LONG_TIMEOUT_COMMANDS = ("R", "CAL")
-    SLEEP_COMMANDS = ("SLEEP", )
+    SLEEP_COMMANDS = ("SLEEP",)
 
     @property
     def bus_number(self):
@@ -303,7 +304,9 @@ class AtlasI2c:
         # else:
         #     return "Error " + str(response[0])
         if num_of_bytes > 1:
-            return self._smbus.read_i2c_block_data(self._address, register, num_of_bytes)
+            return self._smbus.read_i2c_block_data(
+                self._address, register, num_of_bytes
+            )
         else:
             return self._smbus.read_byte_data(self._address, register, num_of_bytes)
 
@@ -311,7 +314,7 @@ class AtlasI2c:
         # appends the null character and sends the string over I2C
         cmd += "\00"
         print(cmd)
-        self.file_write.write(cmd.encode('latin-1'))
+        self.file_write.write(cmd.encode("latin-1"))
 
     # def close(self):
     #     self.file_read.close()
@@ -339,29 +342,68 @@ class AtlasI2c:
 
 
 class _CommonsI2c:
-
     def __init__(self, device):
         self._device = device
+
+    def _check_module_type(moduletype):
+        if moduletype not in ["EC", "PH"]:
+            raise Exception("sorry i can just read device info for EC or PH moduletype")
+        else:
+            return True
+
+    def _convert_raw_hex_ec(byte_array):
+        """
+        @brief convert ec bytearray response to float result
+        return float EC in µs
+        """
+        return float.fromhex(byte_array.hex()) / 100
+
+    def _convert_raw_hex_ph(byte_array):
+        """
+        @brief convert ph bytearray response to float result
+        return float pH
+        """
+        return float.fromhex(byte_array.hex()) / 1.000
 
     """
     Getters commons methods
     """
+
     def get_device_info(self):
         """
         @brief Get device information
         @param device = AltasI2c instance
         @return device name, firmware version
         """
-        if self._device.moduletype not in ["EC", "PH"]:
-            return "sorry i can just read device info for EC or PH moduletype"
-        elif("EC" == self._device.moduletype
-             or "PH" == self._device.moduletype
-             ):
-            info = self._device.read(
-                self._device.OEM_EC_REGISTERS['device_type'],
-                self._device.TWO_BYTE_READ
+        if self._check_module_type(self._device.moduletype):
+            if "EC" == self._device.moduletype or "PH" == self._device.moduletype:
+                info = self._device.read(
+                    self._device.OEM_EC_REGISTERS["device_type"],
+                    self._device.TWO_BYTE_READ,
+                )
+            return "SUCCESS: %s, module type: %s and firmware is: %s" % (
+                self._device.moduletype,
+                info[0],
+                info[1],
             )
-        return "SUCCESS: module type: %s and firmware is: %s" % (info[0], info[1])
+
+    def get_type(self):
+        if self._check_module_type(self._device.moduletype):
+            if "EC" == self._device.moduletype or "PH" == self._device.moduletype:
+                device_type = self._device.read(
+                    self._device.OEM_EC_REGISTERS["device_type"],
+                    self._device.ONE_BYTE_READ,
+                )
+            return device_type
+
+    def get_firmware(self):
+        if self._check_module_type(self._device.moduletype):
+            if "EC" == self._device.moduletype or "PH" == self._device.moduletype:
+                firmware = self._device.read(
+                    self._device.OEM_EC_REGISTERS["device_firmware"],
+                    self._device.ONE_BYTE_READ,
+                )
+            return firmware
 
     def get_read(self):
         """
@@ -369,21 +411,19 @@ class _CommonsI2c:
         @param device = AltasI2c instance
         @return string (depending of O parameter)
         """
-        if self._device.moduletype not in ["EC", "PH"]:
-            return "sorry i can just read device info for EC or PH moduletype"
-        elif "EC" == self._device.moduletype:
-            rawhex = self._device.read(
-                self._device.OEM_EC_REGISTERS['device_ec_msb'],
-                self._device.FOUR_BYTE_READ
-            )
-            print(rawhex)
-
-        elif "PH" == self._device.moduletype:
-            rawhex = self._device.read(
-                self._device.OEM_PH_REGISTERS['device_ec_msb'],
-                self._device.FOUR_BYTE_READ
-            )
-            print(rawhex)
+        if self._check_module_type(self._device.moduletype):
+            if "EC" == self._device.moduletype:
+                rawhex = self._device.read(
+                    self._device.OEM_EC_REGISTERS["device_ec_msb"],
+                    self._device.FOUR_BYTE_READ,
+                )
+                return self._convert_raw_hex_ec(rawhex)
+            elif "PH" == self._device.moduletype:
+                rawhex = self._device.read(
+                    self._device.OEM_PH_REGISTERS["device_ec_msb"],
+                    self._device.FOUR_BYTE_READ,
+                )
+                return self._convert_raw_hex_ph(rawhex)
 
     def get_temperature(self):
         """
@@ -435,6 +475,7 @@ class _CommonsI2c:
 
     """ Setters commons methods
     """
+
     def set_temperature(self, t=25.0):
         """
         @brief Set the compensation temperature
@@ -476,11 +517,15 @@ class _CommonsI2c:
             return "only decimal address expected, convert hexa by using \
                 AtlasI2c.ADDR_OEM_DECIMAL or AtlasI2c.ADDR_EZO_DECIMAL"
         else:
-            if(add not in AtlasI2c.ADDR_OEM_DECIMAL
+            if (
+                add not in AtlasI2c.ADDR_OEM_DECIMAL
                 and add not in AtlasI2c.ADDR_EZO_DECIMAL
-               ):
-                return "cannot use this i2c address %d check \
-                    AtlasI2c.ADDR_OEM_DECIMAL or AtlasI2c.ADDR_EZO_DECIMAL" % add
+            ):
+                return (
+                    "cannot use this i2c address %d check \
+                    AtlasI2c.ADDR_OEM_DECIMAL or AtlasI2c.ADDR_EZO_DECIMAL"
+                    % add
+                )
             else:
                 """
                 write workflow to change physical i2c address
@@ -524,8 +569,10 @@ class ECI2c(_CommonsI2c):
     """
     @brief specific methods for EZO EC module
     """
+
     """ Getters EC methods
     """
+
     def get_k_probe(self):
         """
         @brief Get current ec probe k
@@ -551,6 +598,7 @@ class ECI2c(_CommonsI2c):
     """
     Setters EC methods
     """
+
     def set_k_probe(self, k):
         """
         @brief Set the ec probe k
@@ -580,15 +628,23 @@ class ECI2c(_CommonsI2c):
         @param string EC/TDS/S/SG
         @param state int or bool 1 = enable / 0 = disable
         """
-        return self._device.query("O,%s,%d" % (param, state,))
+        return self._device.query(
+            "O,%s,%d"
+            % (
+                param,
+                state,
+            )
+        )
 
 
 class PHI2c(_CommonsI2c):
     """
     @brief specific methods for EZO PH module
     """
+
     """ Getters pH methods
     """
+
     def get_slope_probe(self):
         """
         @brief Get the pH probe slope
@@ -601,6 +657,7 @@ class PHI2c(_CommonsI2c):
 
     """ Setters pH methods
     """
+
     def set_calibration_mid(self, solution=7.00):
         """
         @brief Calibration the middle point
